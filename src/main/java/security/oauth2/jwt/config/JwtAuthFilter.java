aae29e10f976fab9bfff275015179cd122c83611
NikolayPobelynskiy <npobelynskiy@gmail.com>
2024-05-22 23:37:41 +0300

    login user and add to spring context

--- src/main/java/security/oauth2/jwt/config/JwtAuthFilter.java
+++ src/main/java/security/oauth2/jwt/config/JwtAuthFilter.java
@@ -4,45 +4,46 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import security.oauth2.jwt.service.UserService;
 import security.oauth2.jwt.utils.JwtTokenUtil;
 
 import java.io.IOException;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
-        filterChain.doFilter(request, response);
 
-
-
-        if (!authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
-            System.out.println(authHeader);
+        if ((authHeader != null) && authHeader.startsWith("Bearer ")) {
             authHeader = authHeader.substring(7);
-
             JwtTokenUtil util = new JwtTokenUtil();
-            util.parseToken(authHeader);
-           // System.out.println(util.parseToken(authHeader));
+            String userName = util.getUserNameByToken(authHeader);
+            UserDetails userDetail = userService.loadUserByUsername(userName);
+
+            SecurityContext context = SecurityContextHolder.getContext();
+            Authentication authentication = context.getAuthentication();
+            
+            UsernamePasswordAuthenticationToken authReq
+                    = new UsernamePasswordAuthenticationToken(userDetail.getUsername(), userDetail.getPassword());
+            SecurityContext sc = SecurityContextHolder.getContext();
+            sc.setAuthentication(authReq);
+
+            //Authentication ath = SecurityContextHolder.getContext().getAuthentication();
         }
 
-        System.out.println(SecurityContextHolder.getContext().getAuthentication());
-
-        if (SecurityContextHolder.getContext().getAuthentication() == null) {
-            if (!authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
-                System.out.println(authHeader);
-                authHeader = authHeader.substring(7);
-                System.out.println(authHeader);
-            }
-
-
-            // username, null, listof roles
-           // UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken();
-            // SecurityContextHolder.getContext().setAuthentication(token);
-        }
+        filterChain.doFilter(request, response);
     }
 }
