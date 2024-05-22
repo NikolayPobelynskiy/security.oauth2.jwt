7dc1d110a3f0cc0b9f30940edc5a8092e4f924cd
NikolayPobelynskiy <npobelynskiy@gmail.com>
2024-05-22 23:42:59 +0300

    login user and add to spring context: changes to file JwtTokenUtil parse token to user data

--- src/main/java/security/oauth2/jwt/utils/JwtTokenUtil.java
+++ src/main/java/security/oauth2/jwt/utils/JwtTokenUtil.java
@@ -1,13 +1,12 @@
 package security.oauth2.jwt.utils;
 
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.crypto.SecretKey;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +22,7 @@ public class JwtTokenUtil {
 
     public String generateToken(UserDetails userDetails)
     {
-        long lifeTime = 10 * 60 * 60 * 60L;
+        long lifeTime = 100 * 10 * 60 * 60 * 60L;
 
         Date isseDate = new Date();
         Date endDate = new Date(isseDate.getTime() + lifeTime);
@@ -39,27 +38,26 @@ public class JwtTokenUtil {
                 .compact();
     }
 
-    public void parseToken(String token)
+    public Jws<Claims> tokenToClaims(String token)
     {
         SecretKey secretKeySpec = getSigningKey();
-        JwtParser jwtParser = Jwts.parser()
-                .verifyWith(secretKeySpec)
-                .build();
+        return Jwts.parser().verifyWith(secretKeySpec).build().parseSignedClaims(token);
+    }
 
-        Jwt<?, ?> res = jwtParser.parse(token);
+    public String getUserNameByToken(String token)
+    {
+        Jws<Claims> claims = tokenToClaims(token);
 
-       // Object t = Jwts.parser().verifyWith(secretKeySpec).build().parseSignedClaims(token).getPayload();
-        System.out.println(Jwts.parser().verifyWith(secretKeySpec).build().parseSignedClaims(token).getPayload().get("sub"));
+        return claims.getPayload().get("sub").toString();
+    }
 
-        System.out.println(res.getPayload());
-        // List payload = (List)res.getPayload();
-        // System.out.println(payload.get(0));
-        // https://www.baeldung.com/java-jwt-token-decode
-        // https://www.baeldung.com/java-json-web-tokens-jjwt
-        // http://localhost:8080/public?id=1
-        // http://localhost:8080/parse-token?token=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJudm9udiIsImlhdCI6MTcwOTA2OTY4MSwiZXhwIjoxNzA5MDY5NzE3fQ.zLZXh1hywmNskfDrAhWzpaqo3KcQvZy2nIpwcVxewtk
-// header={alg=HS256},payload={sub=nvonv, iat=1709069681, exp=1709069717},signature=zLZXh1hywmNskfDrAhWzpaqo3KcQvZy2nIpwcVxewtk
+    public List getUserRolesByToken(String token)
+    {
+        Jws<Claims> claims = tokenToClaims(token);
+        
+        List roles = new ArrayList<String>();
+        roles.add(claims.getPayload().get("roles").toString());
 
-//        Jwts.parser().decryptWith(secret.getBytes())
+        return roles;
     }
 }
